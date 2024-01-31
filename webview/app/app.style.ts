691dddb7d67cf32d842ba793f14ba50fc9b05e60
MaxChang3 <maxcfoo@gmail.com>
2024-02-01 01:43:40 +0800

    Remove overflow property from app.style.ts

--- webview/app/app.style.ts
+++ webview/app/app.style.ts
@@ -7,7 +7,6 @@ export const styles = css`
         display: flex;
         height: 100vh;
         justify-content: space-between;
-        overflow: hidden;
     }
     .editor {
         width: 49%;
