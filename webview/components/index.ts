c30781b2c5ab4efefba92ef823316659d187f295
MaxChang3 <maxcfoo@gmail.com>
2024-02-16 23:17:30 +0800

    chore: remove imports

--- webview/components/index.ts
+++ webview/components/index.ts
@@ -1,3 +1,2 @@
 export { vscodeTaskSelector, TaskSelector } from "./task-selector/task-selector"
-export { vsCodeTextArea, TextArea } from "./text-area/text-area"
 export { vscodeCodeMirror, CodeMirror } from "./codemirror/codemirror"
--- webview/main.ts
+++ webview/main.ts
@@ -1,10 +1,9 @@
 import { provideVSCodeDesignSystem, vsCodeDropdown, vsCodeOption, vsCodeProgressRing } from "@vscode/webview-ui-toolkit"
-import { vsCodeTextArea, vscodeTaskSelector, vscodeCodeMirror } from "./components"
+import { vscodeTaskSelector, vscodeCodeMirror } from "./components"
 import { app } from "./app/app"
 
 provideVSCodeDesignSystem().register(
     vscodeCodeMirror(),
-    vsCodeTextArea(),
     vsCodeDropdown(),
     vsCodeOption(),
     vscodeTaskSelector(),
